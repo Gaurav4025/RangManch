2ddfa3bfdc2cd93614d3012aafb79ed9d37c93c5
Gaurav4025 <gauravheh@gmail.com>
2026-09-07 12:33:05 +0530

    delete route

--- routes/reviews.py
+++ routes/reviews.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, HTTPException
 from sqlmodel import Session, select, func
 from models import Review, ReviewCreate, ReviewRead, ReviewUpdate
 from database import get_session
@@ -29,3 +29,59 @@ def list_reviews(
     reviews = session.exec(query).all()
     return reviews
 
+
+@router.get("/average/{play_name}")
+def get_average_rating(play_name: str,  session: Session = Depends(get_session)):
+    result = session.exec(
+        select(func.avg(Review.rating), func.count(Review.id)).where(
+            Review.play_name == play_name
+        )
+    ).first()
+    
+    avg_rating, total_reviews = result
+    
+    if total_reviews == 0:
+        raise HTTPException(status_code = 404, detail = f"No review found for {play_name}")
+    
+    return {
+        "play_name": play_name,
+        "average_rating": round(avg_rating, 2),
+        "total_reviews" : total_reviews
+    }
+    
+
+@router.get("/{review_id}", response_model = ReviewRead)
+def get_review(review_id:int, session: Session = Depends(get_session)):
+    review = session.get(Review, review_id)
+    if not review:
+        raise HTTPException(status_code =  404, detail = "No review")
+    return review
+    
+    
+@router.patch("/{review_id}", response_model = ReviewRead)
+def update_review(review_id:int,update: ReviewUpdate, session: Session = Depends(get_session)):
+    review = session.get(Review, review_id)
+    if not review:
+        raise HTTPException(status_code =  404, detail = "No review")
+    
+    update_data = update.model_dump(exclude_unset = True)
+    for key, value in update_data.items():
+        setattr(review, key, value)
+        
+    session.add(review)
+    session.commit()
+    session.refresh(review)
+    return review
+
+@router.delete("/{review_id}")
+def delete_review(review_id:int, session: Session = Depends(get_session)):
+    review = session.get(Review, review_id)
+    if not review:
+        raise HTTPException(status_code =  404, detail = "No review")
+        
+    session.delete(review)
+    session.commit()
+    return {"message": "Review deleted"}
+            
+
+            
